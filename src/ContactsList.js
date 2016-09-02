578a249d4f577a565e6ea5df9da6dcf6095edbc6
Richard Moss <richard@englishtutorslive.com>
2016-09-02 13:38:05 +0100

    import/export new child component

---
+++ src/ContactsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ContactsList extends React.Component {
+    render() {
+      return (
+        <ul>
+          <li>Ric 555 555 5555</li>
+        </ul>
+      );
+    }
+}
+
+export default ContactsList;
