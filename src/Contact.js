b424bb36c8682ac7075bd82dc47f43c52acd056b
Richard Moss <richard@englishtutorslive.com>
2016-09-05 14:49:50 +0100

    adds search filter

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class Contact extends React.Component {
     render() {
-      console.log(this.props.contacts);
+      // console.log(this.props.contacts);
       return (
           <li>
             {this.props.contact.name} {this.props.contact.phone}
--- src/ContactsList.js
+++ src/ContactsList.js
@@ -8,7 +8,7 @@ class ContactsList extends React.Component {
     constructor() {
       super();
       this.state = {
-        search: 'Level Up'
+        search: ''
       };
     }
 
@@ -17,17 +17,21 @@ class ContactsList extends React.Component {
     }
 
     render() {
-      console.log(this.props.contacts);
+      let filteredContacts = this.props.contacts.filter(
+        (contact) => {
+          return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+        }
+      );
       return (
         <div>
+        <input type="text"
+        value={this.state.search}
+        onChange={this.updateSearch.bind(this)}/>
           <ul>
-            {this.props.contacts.map((contact)=> {
+            {filteredContacts.map((contact)=> {
               return <Contact contact={contact} key={contact.id} />;
             })}
           </ul>
-          <input type="text"
-          value={this.state.search}
-          onChange={this.updateSearch.bind(this)}/>
         </div>
       );
     }
